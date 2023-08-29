5f79a9e3ac1591db01f67f013ca2d6659668e771
pholica <pholica@redhat.com>
2023-08-29 10:33:36 +0200

    Run main function in __main__ (#44)
    Also move the main definition to __init__.
    The purpose of the __main__ is to be executed when the module is
    executed via python -m tesar.

---
+++ src/tesar/__init__.py
@@ -0,0 +1,23 @@
+import contextlib
+import sys
+
+from dispatch import get_arguments, get_datetime
+
+ARGS = get_arguments()
+
+
+def main():
+    if ARGS.action == "test":
+        from dispatch.__main__ import main as dispatch
+
+        if ARGS.log:
+            datetime_str = get_datetime()
+            with open(f"./artifactlog_{datetime_str}", "a") as log:
+                with contextlib.redirect_stdout(log):
+                    sys.exit(dispatch())
+        else:
+            sys.exit(dispatch())
+    elif ARGS.action == "report":
+        from report.__main__ import main as report
+
+        sys.exit(report())
